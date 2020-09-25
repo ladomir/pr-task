3cf86343ac175ee379c895ae9c62d96117434fcc
ladomir <i.zhuravlev2603@gmail.com>
2020-09-25 20:52:29 +0300

    task_8_20

---
+++ src/com/company/kanel_fraiman/section_8/task_8_21.java
@@ -0,0 +1,28 @@
+package com.company.kanel_fraiman.section_8;
+
+import java.util.Scanner;
+
+public class task_8_21 {
+    /*
+    Напишите класс, который принимает с клавиатуры положительное
+    целое число и выводит на экран ближайшее к нему «крутлое число».
+    Например, для введенного значения 433 выводится на экран 430, а для
+    56 выводится 60.
+     */
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a, b, c = 0;
+            System.out.println("Введите целое число:");
+        a = scanner.nextInt();
+
+        b = a % 10;
+
+        if (b < 5) {
+            c = (a / 10) * 10;
+        } else {
+            c = (a / 10 + 1) * 10;
+        }
+        System.out.println(c);
+    }
+}
