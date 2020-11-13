ef70f4ebc51e764a31514eb315d0b737f15a7c02
ladomir <i.zhuravlev2603@gmail.com>
2020-11-13 18:43:34 +0200

    task_9_30

---
+++ src/com/company/kanel_fraiman/section_9/task_9_30.java
@@ -0,0 +1,32 @@
+package com.company.kanel_fraiman.section_9;
+
+import java.util.Scanner;
+
+public class task_9_30 {
+    /*
+    Напишите класс, который принимает с клавиатуры трехзначное число
+    и проверяет, на какое число из составляющих его цифр оно делится
+    (без остатка).
+    Следует учесть возможность того, что одной из цифр в проверяемом
+    числе может быть и 0.
+     */
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a, a1, a2, a3;
+            System.out.println("Введите трёхзначное число a:");
+        a = scanner.nextInt();
+
+        a1 = a / 100;
+        a2 = (a % 100) / 10;
+        a3 = a % 10;
+
+//        if (a1 != a2 && a1 != a3 && a2 != a3) {
+//            System.out.println("Число содержит три разные цифры");
+//        } else if (a1 == a2 && a1 == a3) {
+//            System.out.println("Все цифры одинаковые");
+//        } else {
+//            System.out.println("Число содержит две разные цифры");
+//        }
+    }
+}
