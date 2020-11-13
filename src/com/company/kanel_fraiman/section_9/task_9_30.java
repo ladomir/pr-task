8bac64f36c008145a39ed7c5e3b65499ecf4040f
ladomir <i.zhuravlev2603@gmail.com>
2020-11-13 21:08:04 +0200

    task_9_30_2

--- src/com/company/kanel_fraiman/section_9/task_9_30.java
+++ src/com/company/kanel_fraiman/section_9/task_9_30.java
@@ -21,12 +21,18 @@ public class task_9_30 {
         a2 = (a % 100) / 10;
         a3 = a % 10;
 
-//        if (a1 != a2 && a1 != a3 && a2 != a3) {
-//            System.out.println("Число содержит три разные цифры");
-//        } else if (a1 == a2 && a1 == a3) {
-//            System.out.println("Все цифры одинаковые");
-//        } else {
-//            System.out.println("Число содержит две разные цифры");
-//        }
+        if (a % a1 == 0) {
+            System.out.printf("Число без отстака делится на %d\n", a1);
+        } if (a % a2 == 0) {
+            System.out.printf("Число без отстака делится на %d\n", a2);
+        } if (a % a3 == 0) {
+            System.out.printf("Число без отстака делится на %d\n", a3);
+        } if (a1 == 0) {
+            System.out.printf("Число %d равно 0\n", a1);
+        } if (a2 == 0) {
+            System.out.printf("Число %d равно 0\n", a2);
+        } if (a3 == 0) {
+            System.out.printf("Число %d равно 0\n", a3);
+        }
     }
 }
