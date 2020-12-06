7895d56c2b961a4b68c3d4df967f173e0b953553
ladomir <i.zhuravlev2603@gmail.com>
2020-12-06 16:13:56 +0200

    task_10_10

---
+++ src/com/company/kanel_fraiman/section_10/task_10_10.java
@@ -0,0 +1,32 @@
+package com.company.kanel_fraiman.section_10;
+
+public class task_10_10 {
+    /*
+    Напиши класс, представляющий версию игры из предыдущего задания
+    - только в этом случае компьютер должен играть сам с собой, то
+    есть и первое значение должно не вводиться с клавиатуры, а генерироваться
+    классом.
+     */
+    public static void main(String[] args) {
+
+        int a, b;
+
+        a = (int) (Math.random() * 100);
+
+        System.out.printf("Определённое компьютером число - центр диапазона равно %d\n", a);
+
+        b = (int) (Math.random() * (((a + 5) - (a - 5)) + 1) + (a - 5));
+
+        System.out.printf("Определённое компьютером число равно %d\n", b);
+
+        if (b == a) {
+            System.out.println("Угадано!");
+        }
+        if (b > a) {
+            System.out.println("Слишком большое!");
+        }
+        if (b < a) {
+            System.out.println("Слишком маленькое!");
+        }
+    }
+}
