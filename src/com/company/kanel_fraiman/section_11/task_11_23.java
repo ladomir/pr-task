7b99b599540c46116c87bc51fcc2071452657c1c
ladomir <i.zhuravlev2603@gmail.com>
2021-02-17 22:21:58 +0200

    task_11_23

--- src/com/company/kanel_fraiman/section_11/task_11_23.java
+++ src/com/company/kanel_fraiman/section_11/task_11_23.java
@@ -1,5 +1,7 @@
 package com.company.kanel_fraiman.section_11;
 
+import java.util.Scanner;
+
 public class task_11_23 {
     /*
     Напишите класс, который вначале принимает с клавиатуры целое положительное
@@ -12,5 +14,18 @@ public class task_11_23 {
     и после окончания вывода серии чисел на экран вывода следует
     вывести текстовые сообщения (по выбору).
      */
+    static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+
+        int a;
+            System.out.println("Введите количество членов прогресси a:");
+        a = scanner.nextInt();
+
+            System.out.println("Отображение целых чисел: ");
+        for (int i = 7; i < ((a+1)*4); i+=4) {
+            System.out.print(i + " ");
+        }
+            System.out.println("\nВсе числа отображены.");
+    }
 }
