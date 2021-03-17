feed49e5d5cdf6adb9a997b5c15c1be9b277b658
ladomir <i.zhuravlev2603@gmail.com>
2021-03-17 21:56:42 +0200

    task_11_44

---
+++ src/com/company/kanel_fraiman/section_11/task_11_44.java
@@ -0,0 +1,23 @@
+package com.company.kanel_fraiman.section_11;
+
+public class task_11_44 {
+    /*
+    Напишите класс, который выводит на экран (в строку с пробелами)
+    все двузначные целые положительные числа, в которых и цифра единиц,
+    и цифра десятков делятся на три с одинаковым остатком.
+     */
+    public static void main(String[] args) {
+
+        int i, i1, i2;
+
+        System.out.println("Отображение чисел: ");
+        for (i = 10; i < 100; i++) {
+            i1 = i / 10;
+            i2 = i % 10;
+            if (i1%3==i2%3) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println("\nВсе числа отображены.");
+    }
+}
