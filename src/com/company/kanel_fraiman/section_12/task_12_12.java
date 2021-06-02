5ec0cd1b34d1761eac830b4393e2c5248d233292
ladomir <i.zhuravlev2603@gmail.com>
2021-06-02 22:55:41 +0300

    task_12_12

--- src/com/company/kanel_fraiman/section_12/task_12_12.java
+++ src/com/company/kanel_fraiman/section_12/task_12_12.java
@@ -10,8 +10,37 @@ public class task_12_12 {
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите имена:");
+        System.out.println("Введите дату рождения ученика (\"в форме ХХ месяца ХХХХ года\"):");
 
+        int n = 4, countZ = 0, countV = 0, countL = 0, countO = 0;
+
+        for (int i = 0; i < n; i++) {
+            String s = scanner.nextLine();
+            String substring1 = "января";
+            String substring2 = "февраля";
+            String substring3 = "марта";
+            String substring4 = "апреля";
+            String substring5 = "мая";
+            String substring6 = "июня";
+            String substring7 = "июля";
+            String substring8 = "августа";
+            String substring9 = "сентября";
+            String substring10 = "октября";
+            String substring11 = "ноября";
+            String substring12 = "декабря";
+            if (s.contains(substring12) || s.contains(substring1) || s.contains(substring2)) {
+                countZ++;
+            } if (s.contains(substring3) || s.contains(substring4) || s.contains(substring5)) {
+                countV++;
+            } if (s.contains(substring6) || s.contains(substring7) || s.contains(substring8)) {
+                countV++;
+            } if (s.contains(substring9) || s.contains(substring10) || s.contains(substring11)) {
+                countO++;
+            }
+        }
+        System.out.printf("Количество учеников которые родились: \n\t- зимой - %d, \n\t- весной - %d, \n\t- летом - %d, \n\t- осенью - %d!", countZ, countV, countL, countO);
+
+        /*
         String [] array = new String[3];
 
         for( int i = 0; i < 3; i++) {
@@ -31,5 +60,7 @@ public class task_12_12 {
         for(int i = array.length-1; i >= 0; i--) {
             System.out.print(array[i] + "\t");
         }
+
+         */
     }
 }
