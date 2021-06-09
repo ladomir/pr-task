a16c1b1323a69217bd039fcab1b5ffa9d510e36b
ladomir <i.zhuravlev2603@gmail.com>
2021-06-09 21:30:21 +0300

    task_12_16_черновой вариант

---
+++ src/com/company/kanel_fraiman/section_12/task_12_16.java
@@ -0,0 +1,24 @@
+package com.company.kanel_fraiman.section_12;
+
+import java.util.Scanner;
+
+public class task_12_16 {
+    /*
+    Напишите класс, который принимает с клавиатуры целое положительное число и определяет, является ли оно простым.
+    Класс должен вывести на экран соответствующее текстовое сообщение.
+     */
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int x, count = 0;
+            System.out.println("Введите целое положительное число Х: ");
+        x = scanner.nextInt();
+
+//        if (x%1==0 && x%x==0) {
+//                System.out.println("Введённое число является простым!");
+//        } else {
+//            System.out.println("Введённое число НЕ является простым!");
+//        }
+    }
+}
