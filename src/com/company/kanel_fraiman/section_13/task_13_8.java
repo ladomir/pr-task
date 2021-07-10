52221358af02d0c2239c6eb5b63cb70e5e4d7fc5
ladomir <i.zhuravlev2603@gmail.com>
2021-07-10 16:41:27 +0300

    task_13_8

---
+++ src/com/company/kanel_fraiman/section_13/task_13_8.java
@@ -0,0 +1,36 @@
+package com.company.kanel_fraiman.section_13;
+
+import java.util.Scanner;
+
+public class task_13_8 {
+    /*
+    Назовем «дважды делимым» целое положительное число, если и оно само,
+    и сумма его делителей - делятся (без остатка, разумеется) на количество делителей числа.
+    Напишите класс, который принимает с клавиатуры целое положительное число и проверяет, является ли оно· «дважды делимым».
+     */
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int x, count = 0, sum = 0;
+            System.out.println("Введите целое положительное число Х: ");
+        x = scanner.nextInt();
+
+        System.out.print("Делители: ");
+        for (int i = 1; i <= x; i++) {
+            if (x%i==0) {
+                count++;
+                sum += i;
+                System.out.print(i + "\t");
+            }
+        }
+
+        System.out.println("\nКоличество делителей - " + count + "\nСумма делителей - " + sum);
+
+        if (x/count==0 && sum/count==0) {
+            System.out.println("\nВведённое целое положжительное число является \"дважды делимым\"!");
+        } else {
+            System.out.println("\nВведённое целое положжительное число НЕ является \"дважды делимым\"!");
+        }
+    }
+}
