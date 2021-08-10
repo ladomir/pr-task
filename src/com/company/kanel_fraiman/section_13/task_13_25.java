0f75e7688c616a8db5010d5ccf71608679b23e18
ladomir <i.zhuravlev2603@gmail.com>
2021-08-10 12:51:35 +0300

    task_13_25

---
+++ src/com/company/kanel_fraiman/section_13/task_13_25.java
@@ -0,0 +1,22 @@
+package com.company.kanel_fraiman.section_13;
+
+public class task_13_25 {
+    /*
+    Напишите класс, который создает серию из 25 случайных чисел из диапазона 10 ... 99.
+    Класс будет выводить на экран только те числа серии, которые делятся без остатка на первое число в серии.
+     */
+    public static void main(String[] args) {
+
+        int a = 10, b = 99;
+
+        int [] array = new int [25];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * (b - a + 1) + a);
+
+            if(array[i]%array[0]==0){
+                System.out.print(array[i] + "\t");
+            }
+        }
+    }
+}
