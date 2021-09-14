53d07c715493e67ecb84ead43ea9305745f86c87
ladomir <i.zhuravlev2603@gmail.com>
2021-09-14 09:54:32 +0300

    task_14_1

--- src/com/company/kanel_fraiman/section_14/task_14_1.java
+++ src/com/company/kanel_fraiman/section_14/task_14_1.java
@@ -3,11 +3,13 @@ package com.company.kanel_fraiman.section_14;
 import java.util.Scanner;
 
 public class task_14_1 {
+
     /*
         В соревнованиях по бегу принимали участие 150 учеников школы.
         Напишите класс, который принимает с клавиатуры время, показанное в соревнованиях каждым из учеников,
     выводит на экран наилучшийрезультат.
      */
+
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
