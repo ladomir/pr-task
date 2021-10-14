50810b9ae85a1b4a43db8ce959c8f9ce98e6e826
ladomir <i.zhuravlev2603@gmail.com>
2021-10-14 10:15:25 +0300

    task_15_8

---
+++ src/com/company/kanel_fraiman/section_15/task_15_8.java
@@ -0,0 +1,26 @@
+package com.company.kanel_fraiman.section_15;
+
+import java.util.Scanner;
+
+public class task_15_8 {
+    /*
+    Напишите класс, который принимает с клавиатуры значения, пока их сумма остается меньше 150.
+    По окончании ввода следует вывести на экран информацию о количестве введенных значений и их сумме.
+     */
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int a, n = 0, sum = 0;
+
+        a = scanner.nextInt();
+
+        while (sum < 150) {
+            System.out.println("\n\t-\t" + a);
+            n++;
+            sum+=a;
+            a = scanner.nextInt();
+        }
+        System.out.printf("\nКоличество значений, которое было введено - %d, сумма их значений равна - %d!", n, sum);
+    }
+}
