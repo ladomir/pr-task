464631758a76f9f32321994a5dddc6dfc793ccf7
ladomir <i.zhuravlev2603@gmail.com>
2021-10-18 12:09:01 +0300

    task_15_8n

--- src/com/company/kanel_fraiman/section_15/task_15_8.java
+++ src/com/company/kanel_fraiman/section_15/task_15_8.java
@@ -3,12 +3,10 @@ package com.company.kanel_fraiman.section_15;
 import java.util.Scanner;
 
 public class task_15_8 {
-
     /*
         Напишите класс, который принимает с клавиатуры значения, пока их сумма остается меньше 150.
     По окончании ввода следует вывести на экран информацию о количестве введенных значений и их сумме.
     */
-
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
