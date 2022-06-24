5750d6d00c192359035e895f8e5a607d8446870c
ladomir <i.zhuravlev2603@gmail.com>
2022-06-24 11:06:17 +0300

    task_15_49

---
+++ src/com/company/kanel_fraiman/section_15/task_15_49.java
@@ -0,0 +1,34 @@
+package com.company.kanel_fraiman.section_15;
+
+import java.util.Scanner;
+
+public class task_15_49 {
+    /*
+        Напишите класс, который принимает с клавиатуры оценки, полученные группой учеников,
+    и определяет среднюю арифметическую оценку в группе.
+        Ввод данных завершается, когда очередное вводимое с клавиатуры значение не является оценкой.
+     */
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = 1, n = 0;
+        double s = 0, sum = 0;
+
+//            System.out.println("Введите оценку:");
+//        a = scanner.nextInt();
+
+        while (a >= 1 && a <= 5) {
+                System.out.println("Введите оценку:");
+            a = scanner.nextInt();
+            if (a >= 1 && a <= 5) {
+                sum += a;
+                n++;
+                s = sum / n;
+                    System.out.println("Средняя арифметическая оценка в группе - " + s);
+            } else {
+                System.out.println("Некорректный ввод");
+                System.out.println("Средняя арифметическая оценка в группе - " + s);
+            }
+        }
+    }
+}
