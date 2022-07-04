75dcd32737caa8089c89919153d3d9b8f356b371
ladomir <i.zhuravlev2603@gmail.com>
2022-07-04 10:14:19 +0300

    task_16_1

---
+++ src/com/company/kanel_fraiman/section_16/task_16_1.java
@@ -0,0 +1,18 @@
+package com.company.kanel_fraiman.section_16;
+
+public class task_16_1 {
+    /*
+        Перед вами фрагмент класса:
+    for (int i =О; i <4; i++)
+    for(int j=O; j< 5; j++)
+        System.out.print (i+j);
+    С помощью таблицы трассировки проследите за выполнением фрагмента и запишите, что будет выведено на экран.
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println(i+j);
+            }
+        }
+    }
+}
