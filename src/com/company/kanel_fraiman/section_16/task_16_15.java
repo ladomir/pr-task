c2a083f8dcc77394e7c142c1078072c45c7ead10
ladomir <i.zhuravlev2603@gmail.com>
2022-08-31 13:03:04 +0300

    task_16_14-15

---
+++ src/com/company/kanel_fraiman/section_16/task_16_15.java
@@ -0,0 +1,28 @@
+package com.company.kanel_fraiman.section_16;
+
+import java.util.Scanner;
+
+public class task_16_15 {
+    /*
+    Напишите класс, который выводит на экран все простые трехзначные числа.
+    Класс также должен определить количество и сумму этих чисел.
+     */
+    public static void main(String[] args) {
+        int a = 999, n = 0, sumI = 0;
+        System.out.print("Простые трёхзначные числа: ");
+        for (int i = 100; i <= a; i++) {
+            int sum = 0;
+            for (int j = 1; j < i; j++) {
+                if (i%j==0) {
+                    sum += j;
+                }
+            }
+            if (sum == 1) {
+                n++;
+                sumI += i;
+                System.out.print(i + " ");
+            }
+        }
+        System.out.printf("\nКоличество простых чисел - %d, их сумма - %d", n, sumI);
+    }
+}
