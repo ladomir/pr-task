00ea7233e51bd012b4656a1327cdad25c8a2a0dc
ladomir <i.zhuravlev2603@gmail.com>
2022-11-12 14:43:55 +0200

    task_17_24

---
+++ src/com/company/kanel_fraiman/section_17/task_17_24a.java
@@ -0,0 +1,40 @@
+package com.company.kanel_fraiman.section_17;
+
+import java.util.Scanner;
+
+public class task_17_24a {
+    /*
+    Напишите метод, который принимает в качестве параметров два параметра линейной функции (а и b, для уравнения у=ах+Ь),
+    а также координаты точки на плоскости (х и у). Метод возвращает значение true,
+    если точка лежит на графике функции; в ином случае метод возвращает значение false.
+     */
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int k, b, x, y;
+
+            System.out.println("Введите значение k");
+        k = scanner.nextInt();
+
+            System.out.println("Введите значение b");
+        b = scanner.nextInt();
+
+            System.out.println("Введите значение координаты x");
+        x = scanner.nextInt();
+
+            System.out.println("Введите значение координаты y");
+        y = scanner.nextInt();
+
+        System.out.println(linearEquation(k,b,x,y));
+    }
+
+    public static boolean linearEquation(int k, int b, int x, int y) {
+        int a = k*x+b;
+        if (a == y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
